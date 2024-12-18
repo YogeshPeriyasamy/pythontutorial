1f22ab356822a2c7c12e1e3cd9c1c84d51932d37
Yogeshperiyasamy <yogeshsri1209@gmail.com>
2024-12-18 14:40:59 +0530

    assignment2 done

---
+++ assignment2.py
@@ -0,0 +1,14 @@
+monitor the individual performance
+
+3steps:  ML
+         Supervised
+         Classification
+
+project name:Blacksheepfinder
+
+dummy dataset
+
+ip1(attendance)    ip2(workdone)    ip3(intervaltaken)     op
+100%               95%              2/10                   wont resign
+96%                90%              4/10                   wont resign
+70%                71%              6/10                   will resign
